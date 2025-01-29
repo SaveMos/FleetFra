a3b0f3d1dc24010d3a9284f50f0632a7b8e491cc
Tave21 <iltave4ever@libero.it>
2025-01-29 19:29:47 +0100

    finito Spring

--- src/test/java/it/unipi/dsmt/FleetFra/FleetFraApplicationTests.java
+++ src/test/java/it/unipi/dsmt/FleetFra/FleetFraApplicationTests.java
@@ -1,13 +1,13 @@
 package it.unipi.dsmt.FleetFra;
 
+import it.unipi.dsmt.CaveDowny.FleetFraApplication;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = FleetFraApplication.class)  // Aggiungi questa linea per specificare la classe principale
 class FleetFraApplicationTests {
 
 	@Test
 	void contextLoads() {
 	}
-
 }
