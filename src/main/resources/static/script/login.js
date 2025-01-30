d9a8d05ad71e35b31f315a64309521a929c2711c
gabripian <gabrielepianigiani28@gmail.com>
2025-01-30 23:09:01 +0100

    aggiunti file interfaccia

--- src/main/resources/static/script/login.js
+++ src/main/resources/static/script/login.js
@@ -27,77 +27,138 @@ document.addEventListener('DOMContentLoaded', () => {
     });
 });
 
-document.querySelector('button[class="login"]').addEventListener('click', (e) => {
-    e.preventDefault();
-    let hasError = false;
+$(document).ready(function () {
+    document.querySelector('button[class="login"]').addEventListener('click', (e) => {
+        e.preventDefault();
+        let isEmpty = false;
 
-    // Reset all error messages
-    document.querySelectorAll('.error-message').forEach(msg => msg.style.display = 'none');
+        // Reset all error messages
+        document.querySelectorAll('.error-message').forEach(msg => msg.style.display = 'none');
 
-    // Username validation
-    const username = document.getElementById('login-username').value.trim();
-    if (username.length === 0) {
-        document.getElementById('error-login-username').style.display = 'block';
-        hasError = true;
-    }
+        // Username validation
+        const username = document.getElementById('login-username').value.trim();
+        if (username.length === 0) {
+            document.getElementById('error-login-username').style.display = 'block';
+            isEmpty = true;
+        }
+        // Password validation
+        const password = document.getElementById('login-password').value.trim();
+        if (password.length === 0) {
+            document.getElementById('error-login-password').style.display = 'block';
+            isEmpty = true;
+        }
+        console.log("username = "+username);
+        console.log("password = "+password);
+        // If no errors, submit the form (this is where you can handle the submission logic)
+        if (!isEmpty) {
 
-    // Password validation
-    const password = document.getElementById('login-password').value.trim();
-    if (password.length === 0) {
-        document.getElementById('error-login-password').style.display = 'block';
-        hasError = true;
-    }
+            let requestUser = {
+                username: username,
+                password: password
+            };
 
-    // If no errors, submit the form (this is where you can handle the submission logic)
-    if (!hasError) {
-        console.log('Form submitted successfully!');
-    }
+            $.ajax({
+                url: "http://10.2.1.26:5050/login",
+                data: JSON.stringify(requestUser),
+                type: "POST",
+                dataType: "json",
+                contentType: 'application/json',
+                success: function () {
+
+                    if (username === "admin") {
+                        sessionStorage.setItem("userLog", "admin");
+                        window.location.href = "admin.html";
+
+                    } else {
+                        sessionStorage.setItem("userLog", username);
+                        //sessionStorage.setItem("gameId", "");
+                        window.location.href = "game.html";
+                    }
+
+                },
+                error: function (xhr) {
+                    let errorResponse = JSON.parse(xhr.responseText);
+                    document.getElementById("error-login-password").innerText = errorResponse.answer;
+                    document.getElementById('error-login-password').style.display = 'block';
+
+                }
+            })
+
+        }
+    });
 });
-document.querySelector('button[class="signup"]').addEventListener('click', (e) => {
-    e.preventDefault();
-    let hasError = false;
 
-    // Reset all error messages
-    document.querySelectorAll('.error-message').forEach(msg => msg.style.display = 'none');
+$(document).ready(function () {
+    document.querySelector('button[class="signup"]').addEventListener('click', (e) => {
+        e.preventDefault();
+        let isEmpty = false;
 
-    // Name validation
-    const name = document.getElementById('signup-name').value.trim();
-    if (name.length < 3 || /[^a-zA-Z]/.test(name)) {
-        document.getElementById('error-signup-name').style.display = 'block';
-        hasError = true;
-    }
+        // Reset all error messages
+        document.querySelectorAll('.error-message').forEach(msg => msg.style.display = 'none');
 
-    // Surname validation
-    const surname = document.getElementById('signup-surname').value.trim();
-    if (surname.length < 3 || /[^a-zA-Z]/.test(surname)) {
-        document.getElementById('error-signup-surname').style.display = 'block';
-        hasError = true;
-    }
+        // Name validation
+        const name = document.getElementById('signup-name').value.trim();
+        if (name.length < 3 || /[^a-zA-Z]/.test(name)) {
+            document.getElementById('error-signup-name').style.display = 'block';
+            isEmpty = true;
+        }
 
-    // Username validation
-    const username = document.getElementById('signup-username').value.trim();
-    if (username.length < 5) {
-        document.getElementById('error-signup-username').style.display = 'block';
-        hasError = true;
-    }
+        // Surname validation
+        const surname = document.getElementById('signup-surname').value.trim();
+        if (surname.length < 3 || /[^a-zA-Z]/.test(surname)) {
+            document.getElementById('error-signup-surname').style.display = 'block';
+            isEmpty = true;
+        }
 
-    // Email validation
-    const email = document.getElementById('signup-email').value.trim();
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    if (!emailRegex.test(email)) {
-        document.getElementById('error-signup-email').style.display = 'block';
-        hasError = true;
-    }
+        // Username validation
+        const username = document.getElementById('signup-username').value.trim();
+        if (username.length < 5) {
+            document.getElementById('error-signup-username').style.display = 'block';
+            isEmpty = true;
+        }
 
-    // Password validation
-    const password = document.getElementById('signup-password').value.trim();
-    if (password.length < 5) {
-        document.getElementById('error-signup-password').style.display = 'block';
-        hasError = true;
-    }
+        // Email validation
+        const email = document.getElementById('signup-email').value.trim();
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        if (!emailRegex.test(email)) {
+            document.getElementById('error-signup-email').style.display = 'block';
+            isEmpty = true;
+        }
 
-    // If no errors, submit the form (this is where you can handle the submission logic)
-    if (!hasError) {
-        console.log('Form submitted successfully!');
-    }
+        // Password validation
+        const password = document.getElementById('signup-password').value.trim();
+        if (password.length < 5) {
+            document.getElementById('error-signup-password').style.display = 'block';
+            isEmpty = true;
+        }
+
+        // If no errors, submit the form (this is where you can handle the submission logic)
+        if (!isEmpty) {
+
+            // only a normal user can register, an admin is already registered
+            let person = {
+                firstName: name,
+                lastName: surname,
+                username: username,
+                password: password,
+                email: email
+            };
+
+            $.ajax({
+                url: "http://10.2.1.26:5050/signup",
+                data: JSON.stringify(person),
+                type: "POST",
+                contentType: 'application/json',
+
+                success: function () {
+                    sessionStorage.setItem("userLog", username);
+                    window.location.href = "game.html";
+                },
+                error: function (xhr) {
+                    document.getElementById('error-signup-username').innerText = xhr.responseText;
+                    document.getElementById('error-signup-username').style.display = 'block';
+                }
+            })
+        }
+    });
 });
