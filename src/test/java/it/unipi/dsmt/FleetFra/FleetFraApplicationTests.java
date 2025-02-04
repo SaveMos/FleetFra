a021014330243eb0bc29276f395a746691d7e9cc
Saverio <158563091+SaveMos@users.noreply.github.com>
2025-02-04 16:37:15 +0100

    Client mode created.

--- src/test/java/it/unipi/dsmt/FleetFra/FleetFraApplicationTests.java
+++ src/test/java/it/unipi/dsmt/FleetFra/FleetFraApplicationTests.java
@@ -4,7 +4,7 @@ import it.unipi.dsmt.CaveDowny.FleetFraApplication;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest(classes = FleetFraApplication.class)  // Aggiungi questa linea per specificare la classe principale
+@SpringBootTest(classes = FleetFraApplication.class)
 class FleetFraApplicationTests {
 
 	@Test
