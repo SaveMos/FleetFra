f3f861b4e1e8065d1ff5bf3e14740b04b84bd38d
Saverio <158563091+SaveMos@users.noreply.github.com>
2025-02-05 12:22:13 +0100

    Client mode created.

--- src/main/fleetfra_chin/apps/fleetfra_chin/src/fleetfra_chin_configuration.erl
+++ src/main/fleetfra_chin/apps/fleetfra_chin/src/fleetfra_chin_configuration.erl
@@ -14,11 +14,7 @@
   get_port/0 ,
   get_auto_clean_period/0 ,
   get_max_match_age/0 ,
-  get_battlefield_dimension/0,
-  get_game_info_request_schema_path/0,
-  get_start_game_client_request_schema_path/0 ,
-  get_move_request_schema_path/0,
-  get_start_game_client_request_schema/0
+  get_battlefield_dimension/0
 ]). % Export all functions.
 
 %% get_port/0: Returns the port number for the web server.
@@ -33,23 +29,3 @@ get_auto_clean_period() -> 3600*1000.
 get_max_match_age() -> 24*3600.
 
 get_battlefield_dimension() -> 10.
-
-get_game_info_request_schema_path() -> "json_schema/get_game_info_schema.json".
-
-get_move_request_schema_path() -> "json_schema/move_message_schema.json".
-
-get_start_game_client_request_schema_path() -> "src/json_schema/start_game_client_message_schema.json".
-
-get_start_game_client_request_schema() ->
-  [{<<"game_id">>, [{<<"type">>, <<"string">>}]},
-    {<<"player">>, [{<<"type">>, <<"string">>}]},
-    {<<"player_battlefield">>, [{<<"type">>, <<"array">>},
-      {<<"items">>, [{<<"type">>, <<"object">>},
-        {<<"properties">>, [
-          {<<"col">>, [{<<"type">>, <<"integer">>}]},
-          {<<"row">>, [{<<"type">>, <<"integer">>}]},
-          {<<"value">>, [{<<"type">>, <<"integer">>}]}
-        ]}
-      ]}]},
-    {<<"type_request">>, [{<<"type">>, <<"string">>}]}
-  ].
